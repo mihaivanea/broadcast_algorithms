eeb2108bc2c4fa116387b0ce8a72852eb9303e7f
mihaivanea <mihaivanea1@gmail.com>
2018-01-30 17:40:37 +0000

    Working on System1. Need to update maps when sending and receiving.

--- lib/peer.ex
+++ lib/peer.ex
@@ -3,15 +3,27 @@ defmodule Peer do
   def start() do
     IO.puts(["Peer at ", DNS.my_ip_addr])
     receive do
-      {:bind, neighbours} -> next(neighbours, 0)
+      {:bind, neighbours} -> 
+        sent_to_counts = %{}
+        received_from_counts = %{}
+        for n <- neighbours, do:
+          Map.put(sent_to_counts, n, 0)
+          Map.put(received_from_counts, n, 0)
+        next(neighbours, max_broadcast, timeout, sent_to_counts, received_from_counts)
     end
   end
 
-  defp next(neighbours, count) do
+  defp next(neighbours, max_broadcast, timeout, sent_to_counts, received_from_counts) do
     receive do
-      {:hello} -> forward(neighbours, count) 
-    after 1000 -> 
-      IO.puts(["\t\tPeer with #{Kernel.inspect(self())} received #{count} messages"])
+      {:broadcast, broadcast, timeout} -> 
+        for n <- neighbours, do:
+          send(n, {:msg, self()}) 
+          sent_to_counts = sent_to_counts[n] + 1
+        next(neighbours, max_broadcast - 1, timeout, sent_to_counts, received_from_counts)
+      {:}
+    after timeout -> 
+      puts("Peer #{inspect(self())} ")
+
     end
   end
 
--- lib/system1.ex
+++ lib/system1.ex
@@ -4,11 +4,15 @@ defmodule System1 do
     IO.puts(["System1 at ", DNS.my_ip_addr()])
     peers = [] 
     no_peers = 5
+    max_broadcasts = 1000
+    timeout = 3000
     peers = for _ <- 1..no_peers, do:
       peers ++ Node.spawn(:'node1@container1.localdomain', Peer, :start, [])
     for p <- peers, do: 
       send p, {:bind, peers}
-    send Enum.at(peers, 0), {:hello}
+    # send Enum.at(peers, 0), {:hello}
+    for p <- peers, do:
+      send(p, {:broadcast, max_broadcasts, timeout})
   end
 
 end
